5d49df3972ae4c552c1e3cca04c1500566230e8d
josheschulz <jschulz@toolbox.com>
2014-07-18 21:59:10 -0700

    My hydra setup

--- config.lua
+++ config.lua
@@ -1,67 +1,50 @@
 local config = {}
 
 config.modules = {
-    "arrangement",
     "monitors",
     "repl",
     "updates",
     "reload",
     "arrows",
-    "lock"
+    "lock",
+    "slide",
+    "hop"
 }
 
 -- Maps monitor id -> screen index.
 config.monitors = {
     autodiscover = true,
-    rows = 2
 }
 
 config.autolaunch = true
 
--- Window arrangements.
-config.arrangements = {
-    {
-        name = "zen",
-        alert = true,
-        mash = { "cmd", "ctrl", "alt" },
-        key = "A",
-        arrangement = {
-            {
-                app_title = "^Mail",
-                monitor = 1,
-                position = "full"
-            },
-            {
-                app_title = "^Slack",
-                monitor = 4,
-                position = "left"
-            },
-            {
-                app_title = "^Messages",
-                monitor = 4,
-                position = function(d)
-                    return d:translate_from('bottom_right', {
-                        y = 42,
-                        h = -40
-                    })
-                end
-            },
-            {
-                app_title = "^ChronoMate",
-                monitor = 4,
-                position = function(d)
-                    return d:translate_from('top_right', {
-                        h = 42
-                    })
-                end
-            },
-            {
-                app_title = "^Spotify",
-                monitor = 6,
-                position = "full"
-            }
-        }
+config.lock = {
+   mash = {"cmd"},
+   key = "ESCAPE"
+}
+
+config.arrows = {
+   mash = {"cmd"},
+   keys = {   UP = "top",
+        DOWN = "bottom",
+        LEFT = "left",
+        RIGHT = "right",
+        SPACE = "full",
     }
+}
+
+config.slide = {
+   mash = {"cmd", "ctrl"},
+   step = 50 
+}
+
+config.hop = {
+   mash = {"cmd","shift"},
+   keys = { k = "up",
+    j = "down",
+    h = "left",
+    l = "right",
+   }
 }
 
 return config
